ea58cf93f6199556782f38186002d5e6cc109dc7
Pathin220 <61518695+Pathin220@users.noreply.github.com>
2021-04-22 22:22:04 -0400

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,72 @@
+#include "player.h"
+#include "lands.h"
+
+Player::Player(unsigned int health, unsigned int hunger, unsigned int thirst,
+        unsigned int x, unsigned int y){
+            this->health = health;
+            this->hunger = hunger;
+            this->thirst = thirst;
+            this->x = x;
+            this->y = y;
+        }
+        unsigned int Player::getX() const{
+            return x;
+        }
+        unsigned int Player::getY() const{    
+            return y;
+        }
+        void Player::setX(unsigned int newX){
+            x = newX;
+        }
+        void Player::setY(unsigned int newY){
+            y = newY;
+        }
+        unsigned int Player::getHunger() const{
+            return hunger;
+        }   
+        unsigned int Player::getThirst() const{
+            return thirst;
+        }
+        unsigned int Player::getHealth() const{
+            return health;
+        }
+        void Player::setThirst(unsigned int t){
+            thirst = t;
+        }
+        void Player::setHunger(unsigned int h){
+            hunger = h;
+        }
+        void Player::setHealth(unsigned int h){
+            health = h;
+        }
+        
+         unsigned int Player::newHealth() const {
+            return health;
+        }
+      
+        string Player::printStats(){
+            ostringstream ss;
+            ss << "health: " << health << " " << endl;
+            ss << "hunger: " << hunger << " " << endl;
+            ss << "thirst: " << thirst << " " << endl;
+            return ss.str();
+        }
+        void Player::newTurn(){
+            
+            if( Player::getHunger() != 0){
+            getHealth() - 1;
+            int newHealth;
+            }
+            
+            if( Player::getThirst() != 0){
+            getHealth() - 1;
+            int newHealth;
+        }
+        }
+         bool Player::isAlive()  const {
+            return health > 0;
+        }
+        
+        /*Player::isDead() const {
+            getHealth(=0);
+        }*/
